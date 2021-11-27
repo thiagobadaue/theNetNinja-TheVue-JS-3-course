5fb341167868e62e072e385eadcb573176ac3ae3
thiagobadaue <thiagobadaue@gmail.com>
2021-11-27 19:55:56 -0300

    first commit

---
+++ project-planner/src/views/EditProject.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+    <label>Title:</label>
+    <input type="text" v-model="title" required />
+    <label>Details:</label>
+    <textarea v-model="details" required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data(){
+    return{
+      title:'',
+      details:'',
+      uri: 'http://localhost:3000/projects/'
+    }
+  },
+  mounted(){
+    fetch(this.uri + this.id)
+      .then(res=>res.json())
+      .then(data=>{
+        this.title = data.title;
+        this.details = data.details;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ project-planner/src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+   <div v-if="projects.length">
+     <div v-for="project in projects" :key="project.id">
+        <SingleProject 
+        :project="project" 
+        @delete="handleDelete" 
+        @complete="handleComplete"/>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from '../components/SingleProject.vue'
+
+
+export default {
+  name: 'Home',
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  components: {
+    SingleProject
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+      .then(res=> res.json())
+      .then(data=>this.projects = data)
+      .catch(error=>console.log(error))
+  },
+  methods:{
+    handleDelete(id){
+      this.projects = this.projects.filter((project)=> project.id !== id)
+    },
+    handleComplete(id){
+      let p = this.projects.find(project=> project.id === id)
+      p.complete = !p.complete
+    }
+  }
+}
+</script>
